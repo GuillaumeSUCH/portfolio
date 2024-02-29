59fe67b389b52e2d582ae1ecd3280805aa0654bf
Guillaume SUCH <guillaume.such@gmail.com>
2024-02-29 17:19:07 +0100

    try

--- src/components/weather.tsx
+++
@@ -1,9 +0,0 @@
-// pages/toulon.tsx
-import React from 'react';
-import WeatherComponent from '../components/toulon';
-
-const ToulonPage = () => {
-  return <WeatherComponent />;
-};
-
-export default ToulonPage;
